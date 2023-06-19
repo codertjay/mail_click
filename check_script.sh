78e47cad97741f1a9d7e5e87ad77303c01662004
Codertjay <codertjay@gmail.com>
2023-06-19 17:49:55 +0100

    modify script for cronjob every ten minutes to monitor click

--- check_script.sh
+++ check_script.sh
@@ -8,15 +8,9 @@ is_script_running() {
 # Activate the virtual environment
 source /home/ubuntu/mail_click/venv/bin/activate
 
-# Loop to check and re-run the script every 30 minutes
-while true; do
-    # Check if the script is running
-    if ! is_script_running; then
-        echo "Script is not running. Re-running..."
-        # Run the Python script using nohup and redirect output to nohup.out
-        nohup /home/ubuntu/mail_click/venv/bin/python3 /home/ubuntu/mail_click/click.py > nohup.out 2>&1 &
-    fi
-
-    # Delay for 30 minutes (30 minutes = 1800 seconds)
-    sleep 600
-done
+# Check if the script is running
+if ! is_script_running; then
+    echo "Script is not running. Re-running..."
+    # Run the Python script using nohup and redirect output to nohup.out
+    nohup /home/ubuntu/mail_click/venv/bin/python3 /home/ubuntu/mail_click/click.py > /home/ubuntu/mail_click/nohup.log 2>&1 &
+fi
